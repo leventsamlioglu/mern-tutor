eb2493d3377143457cc46aa6267b5ae2b72d7fc7
leventsamlioglu <leventsamlioglu@icloud.com>
2023-10-03 20:33:28 +0200

    add userController

---
+++ backend/controllers/userController.js
@@ -0,0 +1,15 @@
+const registerUser = (req, res) => {
+	res.json({ message: "Register User" });
+};
+const loginUser = (req, res) => {
+	res.json({ message: "Login User" });
+};
+const getMe = (req, res) => {
+	res.json({ message: "User Data Display" });
+};
+
+module.exports = {
+	registerUser,
+	loginUser,
+	getMe,
+};
